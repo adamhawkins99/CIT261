e275594f61975fed4d079d5cfc9a56e549d90e2e
Adam Hawkins <adamhawkins99@gmail.com>
2019-09-30 07:44:09 -0700

    Adding Basic Java for Week 1

---
+++ BasicJava/src/main/java/BasicJava1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AdamHawkinsPC
+ */
+public class BasicJava1 {
+ public static void main(String args[]){
+
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to CIT261");
+            System.out.println("Java is Awesome");
+
+
+        }
+}
